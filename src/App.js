a7f413523293e7656813464e63760485d099ffac
Mani Wadhwa <maniwadhwa26@gmail.com>
2019-01-23 14:54:21 +0530

    Pomodoro timer initial commit

--- src/App.js
+++ src/App.js
@@ -3,26 +3,149 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  constructor() {
+      super();
+
+      // Initial State
+      this.state = {
+        alert: {
+          type: '',
+          message: ''
+        },
+        time: 0
+      };
+
+      // Defined times for work, short break and long break...
+      this.times = {
+        defaultTime: 1500, // 25 min
+        shortBreak: 300, // 5 min
+        longBreak: 900 // 15 min
+      };
+    }
+
+    componentDidMount() {
+      // Set default time when the component mounts
+      this.setDefaultTime();
+    }
+
+    setDefaultTime = () => {
+      // Default time is 25 min
+      this.setState({
+        time: this.times.defaultTime
+      });
+    }
+
+    setTime = newTime => {
+     
+
+      this.setState({
+        time: newTime
+      });
+       this.restartInterval();
+    }
+
+    restartInterval = () => {
+      // Clearing the interval
+      clearInterval(this.interval);
+
+      // Execute countDown every second
+      this.interval = setInterval(this.countDown, 1000);
+    }
+
+    countDown = () => {
+      // If the time reach 0 then we display Buzzzz! alert.
+      if (this.state.time === 0) {
+        this.setState({
+          alert: {
+            type: 'buz',
+            message: 'Buzzzzzzzz!'
+          }
+        });
+      } else {
+        // We decrease the time second by second
+        this.setState({
+          time: this.state.time - 1
+        });
+      }
+    }
+
+    setTimeForWork = () => {
+      this.setState({
+        alert: {
+          type: 'work',
+          message: 'Working!'
+        }
+      });
+
+      return this.setTime(this.times.defaultTime);
+    }
+
+    setTimeForShortBreak = () => {
+      this.setState({
+        alert: {
+          type: 'shortBreak',
+          message: 'Taking a Short Break!'
+        }
+      });
+
+      return this.setTime(this.times.shortBreak);
+    }
+
+    setTimeForLongBreak = () => {
+      this.setState({
+        alert: {
+          type: 'longBreak',
+          message: 'Taking a Long Break!'
+        }
+      });
+
+      return this.setTime(this.times.longBreak);
+    }
+
+    displayTimer(seconds) {
+      // Formatting the time into mm:ss
+      const m = Math.floor(seconds % 3600 / 60);
+      const s = Math.floor(seconds % 3600 % 60);
+
+      return `${m < 10 ? '0' : ''}${m}:${s < 10 ? '0' : ''}${s}`;
+    }
+
+    render() {
+      const { alert: { message, type }, time } = this.state;
+
+      return (
+        <div className="Pomodoro">
+          <div className={`alert ${type}`}>
+            {message}
+          </div>
+
+          <div className="timer">
+            {this.displayTimer(time)}
+          </div>
+
+          <div className="types">
+            <button 
+              className="start" 
+              onClick={this.setTimeForWork}
+            >
+              Start Working
+            </button>
+            <button 
+              className="short" 
+              onClick={this.setTimeForShortBreak}
+            >
+              Short Break
+            </button>
+            <button 
+              className="long" 
+              onClick={this.setTimeForLongBreak}
+            >
+              Long Break
+            </button>
+          </div>
+        </div>
+      );
+    }
   }
-}
 
 export default App;
